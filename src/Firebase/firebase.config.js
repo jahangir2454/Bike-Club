e5bf782c15faf6a5047419e9723962c2d3cac30d
Your Name <you@example.com>
2021-11-10 23:07:13 +0600

    env

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBsWy-pE7ozvOHGvRT-lIPZTO99AMIAAyE",
-    authDomain: "bikes-clubs.firebaseapp.com",
-    projectId: "bikes-clubs",
-    storageBucket: "bikes-clubs.appspot.com",
-    messagingSenderId: "1015942935703",
-    appId: "1:1015942935703:web:08c4353583dc6635bda5d2"
+    apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId:process.env.REACT_APP_FIREBASE_APP_ID,
   };
 
   export default firebaseConfig;
